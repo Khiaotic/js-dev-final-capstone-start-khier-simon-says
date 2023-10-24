7e6cacfef7782e4b464e8fe507dc1e22231b35ff
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-10-24 06:25:20 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -412,7 +412,7 @@ function checkPress(color) {
   
   if (computerSequence[index] !== playerSequence[index]) {
 setTimeout(() => 
-    resetGame("Absolutely not. WRONG. TRY AGAIN", 1000)
+    resetGame("Absolutely not. WRONG. TRY AGAIN"), 1000)
     // return
   }
   
